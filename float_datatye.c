ae32fa78e01439b21fb685b4c75a5c698e6257b6
Ben10Byte <rk4096172@gmail.com>
2025-01-24 00:43:36 +0530

    The Volume of Sphere

--- float_datatye.c
+++ float_datatye.c
@@ -3,7 +3,7 @@ int main(){
     float x = 5; 
     float y = 2;
     printf("%f",x/y);
-    printf("\n%f",5/2);
+    printf("\n%f",5/2);// Before sometime that is produce integer type 
 
     return 0;
 }
