466aa181a4846a59bb3aa44e7e801cd5f10af75c
Ben10Byte <rk4096172@gmail.com>
2025-01-24 01:27:51 +0530

    nothing

---
+++ percentage_nothing.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float m1 = 95; // History
+    float m2 = 95; // Social Science
+    float m3 = 95; // Environment Studies
+    float m4 = 95; // Maths
+    float m5 = 95; // English
+    float p = (m1 + m2 + m3 + m4 + m5)/5;
+    printf("Percentage of all subjects : %f",p);
+    return 0;
+}
