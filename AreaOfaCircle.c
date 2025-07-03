cabe38949f81c991de646b6df41d4568a9226799
Ben10Byte <rk4096172@gmail.com>
2025-07-03 17:13:55 +0530

    c language
    decimal

--- AreaOfaCircle.c
+++ AreaOfaCircle.c
@@ -3,7 +3,8 @@
 #include<stdio.h>
 int main(){
     float r = 5;
-    float area = 3.14 * r * r;
+    float pi = 3.1415;
+    float area = pi * r * r;
     printf("Area of a Circle : %f",area);
     return 0;
 }
